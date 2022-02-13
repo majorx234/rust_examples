b11fbbc01e0dc1265416e861a753f6b88d6b9b75
MajorX234 <majorx234@googlemail.com>
2022-02-13 13:54:16 +0100

    Add async connection handling function,add html files

--- src/async_webserver/bin/main.rs
+++ src/async_webserver/bin/main.rs
@@ -1,7 +1,26 @@
+use async_std::fs;
+use async_std::net::TcpListener;
+use async_std::net::TcpStream;
+use async_std::prelude::*;
 use futures::executor::block_on;
 
 async fn handle_connection(mut stream: TcpStream) {
-    //...
+    let get = b"GET / HTTP/1.1\r\n";
+    // need Bufer for first 1024bytes
+    let mut buffer = [0; 1024];
+    stream.read(&mut buffer).await.unwrap();
+
+    // depending on the message send Hello World or 404 error
+    let (status_line, filename) = if buffer.starts_with(get) {
+        ("HTTP/1.1 200 OK\r\n\r\n", "index.html")
+    } else {
+        ("HTTP/1.1 404 NOT FOUND\r\n\r\n", "404.html")
+    };
+    let contents = fs::read_to_string(filename).await.unwrap();
+
+    let response = format!("{}{}", status_line, contents);
+    stream.write(response.as_bytes()).await.unwrap();
+    stream.flush().await.unwrap();
 }
 
 async fn http_listen() {
