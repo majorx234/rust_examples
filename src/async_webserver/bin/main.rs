81949d7c349e808bd4eecd74b310837f42a9aa2b
MajorX234 <majorx234@googlemail.com>
2022-02-13 14:05:43 +0100

    Add on thread for each connection handling, test a sleep statement

--- src/async_webserver/bin/main.rs
+++ src/async_webserver/bin/main.rs
@@ -2,16 +2,22 @@ use async_std::fs;
 use async_std::net::TcpListener;
 use async_std::net::TcpStream;
 use async_std::prelude::*;
-use futures::executor::block_on;
+use async_std::task::{self, block_on, spawn};
+use std::time::Duration;
 
 async fn handle_connection(mut stream: TcpStream) {
-    let get = b"GET / HTTP/1.1\r\n";
     // need Bufer for first 1024bytes
     let mut buffer = [0; 1024];
     stream.read(&mut buffer).await.unwrap();
 
+    let get = b"GET / HTTP/1.1\r\n";
+    let sleep = b"GET /sleep HTTP/1.1\r\n";
+
     // depending on the message send Hello World or 404 error
     let (status_line, filename) = if buffer.starts_with(get) {
+        ("HTTP/1.1 200 OK\r\n\r\n", "index.html")
+    } else if buffer.starts_with(sleep) {
+        task::sleep(Duration::from_secs(5)).await;
         ("HTTP/1.1 200 OK\r\n\r\n", "index.html")
     } else {
         ("HTTP/1.1 404 NOT FOUND\r\n\r\n", "404.html")
@@ -29,7 +35,7 @@ async fn http_listen() {
 
     // wait for incomming connections
     while let Some(tcpstream) = incoming.next().await {
-        handle_connection(tcpstream.unwrap()).await;
+        async_std::task::spawn(handle_connection(tcpstream.unwrap()));
     }
 }
 
