9ef7ef4e570e8b35f94d0028acc36279c52e258d
major <majorx234@googlemail.com>
2022-04-27 11:24:32 +0200

    Add stack implementation

--- src/lib.rs
+++ src/lib.rs
@@ -2,3 +2,32 @@ pub struct Stack<T> {
     max_size: usize,
     items: Vec<T>,
 }
+
+impl<T> Stack<T> {
+    fn new(max: usize) -> Self {
+        Stack {
+            items: Vec::new(),
+            max_size: max,
+        }
+    }
+    pub fn pop(&mut self) -> Option<T> {
+        self.items.pop()
+    }
+    pub fn push(&mut self, item: T) -> bool {
+        if self.items.len() < self.max_size {
+            self.items.push(item);
+            true
+        } else {
+            false
+        }
+    }
+    pub fn is_empty(&self) -> bool {
+        self.items.is_empty()
+    }
+    pub fn length(&self) -> usize {
+        self.items.len()
+    }
+    pub fn top(&self) -> Option<&T> {
+        self.items.last()
+    }
+}
