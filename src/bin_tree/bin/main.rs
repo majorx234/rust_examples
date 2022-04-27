c6e7c2555633c5c913dc144b82ffd272b6a7d075
major <majorx234@googlemail.com>
2022-04-27 22:51:40 +0200

    Add iterative print_tree function

--- src/bin_tree/bin/main.rs
+++ src/bin_tree/bin/main.rs
@@ -1,3 +1,5 @@
+use stack_lib::Stack;
+
 #[derive(Debug, Default)]
 struct Node<T> {
     value: T,
@@ -32,22 +34,63 @@ fn invert_tree(root: &Option<Box<Node<i32>>>) -> Option<Box<Node<i32>>> {
 fn print_tree(root: &Option<Box<Node<i32>>>, level: usize) {
     match root {
         Some(node) => {
-            print_tree(&node.left, level + 1);
+            print_tree(&node.right, level + 1);
             for _ in 0..level {
                 print!("  ");
             }
             println!("{:?}", node.value);
-            print_tree(&node.right, level + 1);
+            print_tree(&node.left, level + 1);
         }
         None => {}
     }
 }
 
+fn print_tree_iterative(root: &Option<Box<Node<i32>>>, level: usize) {
+    let max_size: usize = 32;
+    let mut node_stack: Stack<(i32, u32)> = Stack::new(max_size);
+    let mut tree_stack: Stack<(&Option<Box<Node<i32>>>, u32)> = Stack::new(max_size);
+    tree_stack.push((&root, 0));
+    'stack: loop {
+        let element = tree_stack.pop();
+        match element {
+            Some(stack_elem) => {
+                let (stack_node, deep) = stack_elem;
+                match stack_node {
+                    Some(node) => {
+                        tree_stack.push((&node.left, deep + 1));
+                        tree_stack.push((&node.right, deep + 1));
+                        node_stack.push((node.value, deep));
+                    }
+                    None => {
+                        let value_opt = node_stack.pop();
+                        match value_opt {
+                            Some(value_elem) => {
+                                let (value, deepness) = value_elem;
+                                for _ in 0..deepness {
+                                    print!("  ");
+                                }
+                                println!("{:?}", value);
+                            }
+                            None => {}
+                        };
+                    }
+                }
+            }
+            None => {}
+        };
+        if tree_stack.is_empty() {
+            break 'stack;
+        }
+    }
+}
+
 fn main() {
     let mut counter: i32 = 0;
-    let tree = generate_tree(3, &mut counter);
+    let tree = generate_tree(4, &mut counter);
     // println!("}{:#?}", tree)
     print_tree(&tree, 0);
     let itree = invert_tree(&tree);
     print_tree(&itree, 0);
+    println!("####################");
+    print_tree_iterative(&tree, 0);
 }
