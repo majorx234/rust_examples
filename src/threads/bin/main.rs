e83a795a5bf78b1c8e978ed00741811596a93487
major <majorx234@googlemail.com>
2022-07-16 22:19:25 +0200

    Exchange deprecated method sleep_ms with sleep and duration

--- src/threads/bin/main.rs
+++ src/threads/bin/main.rs
@@ -1,11 +1,11 @@
 use std::sync::mpsc;
-use std::thread;
+use std::{thread,time};
 
 fn main() {
     let (tx, rx) = mpsc::channel();
 
     let sender_thread = thread::spawn(move || {
-        thread::sleep_ms(2000);
+        thread::sleep(time::Duration::from_millis(2000));
         let val = String::from("Ping");
         tx.send(val).unwrap();
     });
@@ -13,14 +13,15 @@ fn main() {
     let receiver_thread = thread::spawn(move || {
         let mut run: bool = true;
         while run {
+            thread::sleep(time::Duration::from_millis(3000));
             let received = rx.recv().unwrap();
             println!("received: {}", received);
             if received == "Ping"{
                 run = false;
-            }
+            }            
         };
         println!("receiver stopped");
     });
 
-    thread::sleep_ms(4000);
+    thread::sleep(time::Duration::from_millis(6000));
 }
