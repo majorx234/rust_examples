60fb03d42411bf17ea005f637af7f82deb325323
major <majorx234@googlemail.com>
2022-08-18 17:14:27 +0200

    Add example with threadfunction inside a closure to use an enclosured parameter

--- src/closures/bin/main.rs
+++ src/closures/bin/main.rs
@@ -1,7 +1,20 @@
+fn thread_function_with_parameters(value: u32) -> () {
+    println!("thread_function_with_parameters: {}", value);
+}
+
 fn main() {
-    let test1: u32 = 42;
+    let test_value1: u32 = 42;
+
+    // thread with closure
     let closure_test_thread1: std::thread::JoinHandle<()> = std::thread::spawn(move || {
-        println!("test1: {}", test1);
+        println!("test_value1: {}", test_value1);
     });
     closure_test_thread1.join().unwrap();
+
+    // thread with closure and thread function with parameters
+    let test_value2: u32 = 43;
+    let closure_test_thread2: std::thread::JoinHandle<()> = std::thread::spawn(move || {
+        thread_function_with_parameters(test_value2);
+    });
+    closure_test_thread2.join().unwrap();
 }
