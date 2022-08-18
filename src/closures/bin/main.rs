52869d9c1623206c9cb93698f0106ec42864e754
major <majorx234@googlemail.com>
2022-08-18 18:26:17 +0200

    Add example where a function gets a closure as parameter

--- src/closures/bin/main.rs
+++ src/closures/bin/main.rs
@@ -2,6 +2,17 @@ fn thread_function_with_parameters(value: u32) -> () {
     println!("thread_function_with_parameters: {}", value);
 }
 
+fn thread_function_with_function_pointer_parameter(
+    value: u32,
+    f_closure: impl Fn(u32) -> u32,
+) -> () {
+    let new_value: u32 = f_closure(value);
+    println!(
+        "thread_function_with_function_pointer_parameter: {}",
+        new_value
+    );
+}
+
 fn main() {
     let test_value1: u32 = 42;
 
@@ -17,4 +28,14 @@ fn main() {
         thread_function_with_parameters(test_value2);
     });
     closure_test_thread2.join().unwrap();
+
+    //thread with closure and thread function with function trait as parameter
+    let test_value3: u32 = 44;
+    let test_value4: u32 = 45;
+    let test_closure = move |a: u32| a + test_value4;
+
+    let closure_test_thread3: std::thread::JoinHandle<()> = std::thread::spawn(move || {
+        thread_function_with_function_pointer_parameter(test_value3, test_closure);
+    });
+    closure_test_thread3.join().unwrap();
 }
